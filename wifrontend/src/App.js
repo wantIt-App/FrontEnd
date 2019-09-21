88a6d8b94f0a255009c1df5177c373f71237e363
Abigayle Franklin <abifanklin@gmail.com>
2019-09-21 10:55:47 -0500

    Menu added to app.js

--- wifrontend/src/App.js
+++ wifrontend/src/App.js
@@ -5,14 +5,55 @@ import MyLists from './Components/mylists'
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { component: undefined }
+  }
+
+  displayLogin=()=>{
+    this.setState({ component: 'Login' })
+  }
+
+  displayMyLists=()=>{
+    this.setState({ component: 'MyLists' })
+  }
+
+  displayListView=()=>{
+    this.setState({ component: 'ListView' })
+  }
+
   render() {
-    return (
-      <div className='constrain'>
-        <Login />
-        <MyLists />
-        <ListView />
-      </div>
-    )
+    if (this.state.component == 'Login' || this.state.component == undefined) {
+      return (
+        <div className='constrain'>
+          <h3>Menu</h3>
+          <button onClick={this.displayLogin}>Login</button>
+          <button onClick={this.displayMyLists}>My Lists</button>
+          <button onClick={this.displayListView}>List View</button>
+          <Login/>
+        </div>
+      )
+    } else if (this.state.component == 'MyLists') {
+      return (
+        <div className='constrain'>
+          <h3>Menu</h3>
+          <button onClick={this.displayLogin}>Login</button>
+          <button onClick={this.displayMyLists}>My Lists</button>
+          <button onClick={this.displayListView}>List View</button>
+          <MyLists/>
+        </div>
+      )
+    } else {
+      return (
+        <div className='constrain'>
+          <h3>Menu</h3>
+          <button onClick={this.displayLogin}>Login</button>
+          <button onClick={this.displayMyLists}>My Lists</button>
+          <button onClick={this.displayListView}>List View</button>
+          <ListView/>
+        </div>
+      )
+    }
   }
 }
 

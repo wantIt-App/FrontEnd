60bd4023f8b2795a7eac8621b80ae71f373f86c7
Abigayle Franklin <abifanklin@gmail.com>
2019-09-21 16:45:51 -0500

    Added ad item modal

--- wifrontend/src/Components/listview.js
+++ wifrontend/src/Components/listview.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Item from './Item'
+import ReactModal from 'react-modal';
 
 let items = [
   {name: 'Lamp',
@@ -22,14 +23,34 @@ let items = [
 class ListView extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { items: items }
+    this.state = { items: items,
+      showModal: false }
+
+  this.handleOpenModal = this.handleOpenModal.bind(this);
+  this.handleCloseModal = this.handleCloseModal.bind(this);
+}
+
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal () {
+    this.setState({ showModal: false });
   }
 
     render() {
       return (
         <div className='List animated fadeIn'>
           <h1>List Name</h1>
+          <button onClick={this.handleOpenModal}>Add Item</button>
+          <ReactModal 
+           isOpen={this.state.showModal}
+           contentLabel="Minimal Modal Example"
+        >
+          <button onClick={this.handleCloseModal}>X</button>
+          <h1>Upload form Here</h1>
           <button>Add Item</button>
+        </ReactModal>
 
           <div className='ListItems animated fadeIn delay-.25s'>
             {this.state.items.map(item => {
